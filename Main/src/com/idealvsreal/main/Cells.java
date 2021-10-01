02331ad32e0d821b125f5db3c0e92dabf04a3698
rmmong2 <rmmong2@gmail.com>
2021-10-01 14:11:15 -0400

    Main method created

---
+++ Main/src/com/idealvsreal/main/Cells.java
@@ -0,0 +1,9 @@
+package com.idealvsreal.main;
+
+public class Cells {
+    String taskName = "";
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+}
--- Main/src/com/idealvsreal/main/HelloWorld.java
+++ Main/src/com/idealvsreal/main/HelloWorld.java
@@ -2,6 +2,5 @@ package com.idealvsreal.main;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
     }
 }
